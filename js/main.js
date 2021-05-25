92fe99dca98d9b97d708cd9dd3948b8e29b73e78
Shravan M <shravanm05@gmail.com>
2021-05-25 13:22:55 +0530

    Added Typeform

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
+
+// Groom School
+
+ (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() 
+
+
 document.getElementById("career_exploration").style.display = "block ";
 
 function changeTrack(id)
